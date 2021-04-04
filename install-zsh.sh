fad14b2104121c6ee125ad2d749decd916f049fe
Niklas Lakner <niklas.lakner@gmail.com>
2021-04-04 21:45:15 +0200

    small fixes

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,7 +1,9 @@
 sudo apt install zsh -y
-chsh -s $(which zsh) -y
+chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+Y
+exit
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
