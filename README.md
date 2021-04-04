26f5a2b30609ed91d7cd9bf05b3df902345b288a
Niklas Lakner <niklas.lakner@gmail.com>
2021-04-04 21:39:00 +0200

    done

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/install-zsh.sh)"
+```
--- install-zsh.sh
+++ install-zsh.sh
@@ -7,3 +7,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.zshrc -o ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.p10k.zsh -o ~/.p10k.zsh
