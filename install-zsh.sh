a1b4b7c11dd68492da92610a91b296566166a43d
Niklas Lakner <niklas.lakner@gmail.com>
2021-04-06 09:00:34 +0200

    test 1

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,3 +1,6 @@
+
+
+
 sudo apt install zsh -y
 chsh -s $(which zsh)
 
@@ -5,8 +8,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.zshrc -o ~/.zshrc
-curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.p10k.zsh -o ~/.p10k.zsh
+curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.zshrc -o $HOME/.zshrc
+curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.p10k.zsh -o $HOME/.p10k.zsh
