eace896ff2b8705a12cba3fea4b8793fa7176ee7
niladi <61806417+niladi@users.noreply.github.com>
2021-06-01 13:24:40 +0200

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,4 +1,22 @@
+#!/bin/sh
 
+while [ "$1" != "" ]; do
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        --skip-rc)
+            SKIP_RC=true
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 
 sudo apt-get install zsh -y
@@ -11,7 +29,11 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.zshrc -o $HOME/.zshrc
+if ! [$SKIP_RC];
+then 
+  curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.zshrc -o $HOME/.zshrc
+fi
+
 curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.p10k.zsh -o $HOME/.p10k.zsh
 
 git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
