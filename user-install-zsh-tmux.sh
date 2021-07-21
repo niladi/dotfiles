f4f1457a1e286c5c6238f9ca547f0811087a3f02
niladi <61806417+niladi@users.noreply.github.com>
2021-07-21 09:55:48 +0200

    Update user-install-zsh-tmux.sh

--- user-install-zsh-tmux.sh
+++ user-install-zsh-tmux.sh
@@ -3,9 +3,10 @@
 # Connect to the server.. 
 # ssh username@server_ip
 mkdir ~/.local;
+WORKDIR=$HOME/tmp
 
 # Download source and build libevent
-cd /tmp;
+cd $WORKDIR;
 wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz;
 tar xvfz libevent-2.1.8-stable.tar.gz;
 cd libevent-2.1.8-stable;
@@ -14,7 +15,7 @@ make;
 make install;
 
 # Download source and build ncurses
-cd /tmp;
+cd $WORKDIR;
 wget http://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz;
 tar xvfz ncurses-6.0.tar.gz;
 cd ncurses-6.0;
@@ -23,7 +24,7 @@ make;
 make install;
 
 # Download source and build zsh
-cd /tmp;
+cd $WORKDIR;
 wget -O zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download;
 mkdir zsh && tar -xvzf zsh.tar.gz -C zsh --strip-components 1;
 cd zsh;
@@ -32,7 +33,7 @@ make;
 make install;
 
 # Download the source and build tmux
-cd /tmp;
+cd $WORKDIR;
 wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz;
 tar xvfz tmux-2.3.tar.gz;
 cd tmux-2.3;
@@ -45,7 +46,7 @@ make;
 # also -static makes it so we don't need to dynamically link shared files in $HOME/.local/lib meaning
 # exporting a LD shell variable. We can just copy it and run it.
 
-cp tmux ~/.local/bin;
+cp tmux $HOME/.local/bin;
 export PATH=$HOME/.local/bin:$PATH;
 
 echo "At this point, you should be able to just type tmux on the command line and have it work."
