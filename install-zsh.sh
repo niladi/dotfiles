e8fa3b2a879fdf85558128038d3100626ed247cf
niladi <61806417+niladi@users.noreply.github.com>
2023-08-29 10:09:43 +0200

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -93,7 +93,5 @@ if [ $SKIP_VIM ];
 then
     echo "not installing awsome vim"
 else
-    mkdir -p $HOME/.vim/pack/themes/start
-    git clone https://github.com/dracula/vim.git $HOME/.vim/pack/themes/start/dracula
-    curl -fsSL https://raw.githubusercontent.com/niladi/zsh-install/master/.vimrc -o $HOME/.vimrc 
+    echo "TODO eigene Vim Config"
 fi
